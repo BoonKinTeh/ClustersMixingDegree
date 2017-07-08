61478973635940777f649e4cc180fc8db46224a0
Teh Boon Kin <Boonkinteh@gmail.com>
2017-07-08 18:51:27 +0800

    Add files via upload

---
+++ AdjustedRandIndex.m
@@ -0,0 +1,62 @@
+function ARI = AdjustedRandIndex(CL_1,CL_2)
+%% Input Description
+% CL_1: Nx1 cell list as Cluster list, each cell represents a cluster with at least one elements
+% CL_2: Nx1 cell list as Cluster list, each cell represents a cluster with at least one elements
+%% Outputs description:
+% ARI: 1x1 double, represents the adjusted Rand Index measured between clusters CL_1 and CL_2,
+%      the global mixing degree hence is 1-ARI
+%
+%% Read Me:
+% This code is Published for "Cluster fusion-fission dynamics in the Singapore stock exchange", 
+% by Boon Kin Teh and Siew Ann Cheong.
+% Please refer to the paper for more details, and cite the paper when you
+% are using this code for significance testing analysis,
+% Thank you.
+
+%% Lastest updated date:
+% 08 July 2017
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Start here %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Contruct Contingency Table
+NumberOfCluster_CL1 = size(CL_1,1);
+NumberOfCluster_CL2 = size(CL_2,1);
+
+ContingencyTable = zeros(NumberOfCluster_CL1,NumberOfCluster_CL2);
+for cl_i = 1:NumberOfCluster_CL1
+    Ecl_i = CL_1{cl_i,1};
+    for cl_j = 1:NumberOfCluster_CL2
+        Ecl_j = CL_2{cl_j,1};
+        ContingencyTable(cl_i,cl_j) = length(intersect(Ecl_i,Ecl_j));
+    end
+end
+%% Measure Index
+Index = 0;
+for cl_i = 1:NumberOfCluster_CL1
+    for cl_j = 1:NumberOfCluster_CL2
+        Index = Index+nchoosek(ContingencyTable(cl_i,cl_j),2);
+    end
+end
+%% Measure Max Index
+MaxIndex = 0;
+for cl_i = 1:NumberOfCluster_CL1
+    MaxIndex = MaxIndex + nchoosek(sum(ContingencyTable(cl_i,:)),2);
+end
+for cl_j = 1:NumberOfCluster_CL2
+    MaxIndex = MaxIndex + nchoosek(sum(ContingencyTable(:,cl_j)),2);
+end
+MaxIndex = MaxIndex/2;
+%% Measure Expected Index
+ExpectedIndex = 0;
+for cl_i = 1:NumberOfCluster_CL1
+    Value = 0;
+    for cl_j = 1:NumberOfCluster_CL2
+        Value = Value+nchoosek(sum(ContingencyTable(:,cl_j)),2);
+    end
+    ExpectedIndex = ExpectedIndex + Value*nchoosek(sum(ContingencyTable(cl_i,:)),2);
+end
+NumberOfElements = sum(sum(ContingencyTable));
+ExpectedIndex = ExpectedIndex/nchoosek(NumberOfElements,2);
+%% Return ARI
+ARI = (Index-ExpectedIndex)/(MaxIndex-ExpectedIndex);
---
+++ DegreeOfMixing.m
@@ -0,0 +1,69 @@
+function [DoM,LocalMixingDegree] = DegreeOfMixing(CL_1,CL_2,k1,k2)
+%% Input Description
+% CL_1: Nx1 cell list as Cluster list, each cell represents a cluster with at least one elements
+% CL_2: Nx1 cell list as Cluster list, each cell represents a cluster with at least one elements
+% k1: an integer represent first k number of clusters are interest for CL_1
+% k2: an integer represent first k number of clusters are interest for CL_2
+%% Outputs description:
+% DoM: 1x1 double, represents the global Degree of Mixing measured between first k1 clusters of CL_1 with
+%      first k2 clusters of CL_2.
+% LocalMixingDegree: 1xk2 double array, indicates the local degree of mixing measured for first k2 clusters in CL_2.
+%% Read Me:
+% This code is Published for "Cluster fusion-fission dynamics in the Singapore stock exchange", 
+% by Boon Kin Teh and Siew Ann Cheong.
+% Please refer to the paper for more details, and cite the paper when you
+% are using this code for significance testing analysis,
+% Thank you.
+
+%% Lastest updated date:
+% 08 July 2017
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Start here %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Determine Elements appeared in both CL_1 and CL_2
+NumberOfElement = 0;
+for cl_i = 1:k1
+    NumberOfElement = NumberOfElement+ length(CL_1{cl_i,1});
+end
+Element_CL1 = zeros(1,NumberOfElement);
+NumberOfElement = 0;
+for cl_i = 1:k1
+    E = CL_1{cl_i,1};
+    Element_CL1(NumberOfElement+1:NumberOfElement+length(E)) = E;
+    NumberOfElement = NumberOfElement + length(E);
+end
+NumberOfElement = 0;
+for cl_j = 1:k2
+    NumberOfElement = NumberOfElement+ length(CL_2{cl_j,1});
+end
+Element_CL2 = zeros(1,NumberOfElement);
+NumberOfElement = 0;
+for cl_j = 1:k2
+    E = CL_2{cl_j,1};
+    Element_CL2(NumberOfElement+1:NumberOfElement+length(E)) = E;
+    NumberOfElement = NumberOfElement + length(E);
+end
+ElementInterested = intersect(Element_CL1,Element_CL2);
+clearvars -except CL_1 CL_2 k1 k2 ElementInterested
+
+%% Determine Degree of Mixing
+LocalMixingDegree = zeros(1,k2);
+for cl_j = 1:k2
+    %%% Determine Local Mixing Degree
+    a= 1/k1; A = (a^3-a^2-a+1/2)/(a^2-a); 
+    f_x = zeros(1,k1);
+    
+    Ecl_j = CL_2{cl_j,1};
+    Ecl_j = intersect(Ecl_j,ElementInterested);
+    for cl_i = 1:k1
+        Ecl_i = CL_1{cl_i,1};
+        Ecl_i = intersect(Ecl_i,ElementInterested);
+        f_x(1,cl_i) = (1+length(intersect(Ecl_i,Ecl_j)))/(1+length(Ecl_i));
+    end
+    f_x = f_x/sum(f_x);
+    f_x = ( f_x.*(f_x-1).*exp(-(A-f_x).^2) ) ./ ( a*(a-1)*exp(-(A-a)^2) );
+    LocalMixingDegree(1,cl_j) = prod(f_x)^a;
+end
+%%% Determine Global Mixing Degree
+DoM = mean(LocalMixingDegree);
